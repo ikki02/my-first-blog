4d924ec57de2a43a7df5e7ab971b454930578c09
ikki02 <B1200315@gl.aiu.ac.jp>
2019-05-03 15:25:24 +0900

    feat: admin for image added

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Image
 
 # To make our model visible on the admin page
 admin.site.register(Post)
+admin.site.register(Image)
